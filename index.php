f195a5c8e7c1067e28e2ef709d5d02168b445cda
Elkin <elkinjoao2004@hotmail.com>
2024-11-28 00:01:10 -0500

    Se agrego el archivo index.css y se realizo su respectiva conexión con index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Cambios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="styles.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
