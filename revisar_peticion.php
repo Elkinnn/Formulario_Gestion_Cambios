68f1454fc4d7e28f32a33fb1840424a799e2b8d7
Diego <diegojijon2005@gmail.com>
2024-11-30 22:29:38 -0500

    Se creo estilos en el archivo revisar_peticion.css y se agrego rutas.

--- revisar_peticion.php
+++ revisar_peticion.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Revisar Peticiones</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
+    <link href="css/revisar_peticion.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Gestión de Cambios</a>
+            <a class="navbar-brand" href="index.php">Gestión de Cambios</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -20,7 +20,7 @@
                         <a class="nav-link" href="enviar_peticion.php">Enviar Petición</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="review.html">Revisar Peticiones</a>
+                        <a class="nav-link" href="revisar_peticion.php">Revisar Peticiones</a>
                     </li>
                 </ul>
             </div>
